30afd95d37f1f035a779e75aaf98feae11e24c3d
Theodore Lodholz <tlodholz@gmail.com>
2025-04-08 09:37:55 -0400

    Add project files.

---
+++ MyWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿namespace MyWebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using MyWebApi.Models;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create a Customer
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+        }
+
+        // Update a Customer
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, [FromBody] Customer customer)
+        {
+            if (id != customer.Id) return BadRequest();
+
+            var existingCustomer = await _context.Customers.FindAsync(id);
+            if (existingCustomer == null) return NotFound();
+
+            existingCustomer.FirstName = customer.FirstName;
+            existingCustomer.LastName = customer.LastName;
+            existingCustomer.Phone = customer.Phone;
+            existingCustomer.Email = customer.Email;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Delete a Customer
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null) return NotFound();
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Retrieve a Customer
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null) return NotFound();
+            return Ok(customer);
+        }
+    }
+
+}
---
+++ MyWebApi/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApi.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Customer> Customers { get; set; }
+}
+
