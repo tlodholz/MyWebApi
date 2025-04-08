9dc361363b4a3733bc9fa38921e6389861bb524f
Theodore Lodholz <tlodholz@gmail.com>
2025-04-08 09:54:05 -0400

    Register DbContext in Program.cs file

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string DbConnectionString = "Server=tcp:web-database-sql.database.windows.net,1433;Initial Catalog=blazor-app-sql;Persist Security Info=True;User ID=database_admin_1;Password=appkey=2025;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -10,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer("YourConnectionStringHere"));
+    options.UseSqlServer(DbConnectionString));
 
 var app = builder.Build();
 
